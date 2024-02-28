e19c79c2abbcbe62b6b8468525d4acd3963df8fd
dwyjmay <dwyjmay@naver.com>
2024-02-28 17:05:10 +0900

    강의5,6 - 컴포넌트 생성과 동적 UI를 만드는 방법(STEP3)

--- src/App.js
+++ src/App.js
@@ -2,16 +2,58 @@
 import './App.css';
 import {useState} from "react";
 
+
+/*
+1. function Name(){return ()}만들기  함수이름은 대문자로 시작.
+2. return()에 html 담기
+3. <함수명></함수명>'
+
+컴포넌트 만들기 좋은 상황
+반복적 html 축약 시
+큰 페이지
+자주 변경되는 UI
+
+단점
+state  가져다 쓸 때 문제가 생긴다
+function  App() 안에서만 정의되어 있기 때문..
+
+컴포넌트 만드는 문법 1
+function Name(){
+return ()
+}
+컴포넌트 만드는 문법 2
+const Modal =  ()=>{
+  return()
+}
+*/
+
+/* 동적인 UI만드는 STEP*/
+/*
+STEP1. html css 로 미리 디자인 완성
+STEP2. UI의 현재 상태를 state로 저장
+STEP3. state에 따라 UI가 어떻게 보일지 작성
+*/
+
+function Modal(){
+    return(
+        <div className="modal">
+            <h4>제목</h4>
+            <p>날짜</p>
+            <p>내용</p>
+        </div>
+    )
+}
+
 function App() {
     const [title,setTitle] = useState(['youth','행복을 빌어줘','21세기의 어떤 날']) ;
     const [like,setLike] = useState(0);
+
+    const [modal,setModal] = useState(false); //STEP2
 
     function myfunc(){
         setLike(like+1);
     }
 
-    /* 숙제 : 첫번재 게시물의 제목을 바꿔보기 */
-    /* array나 object 등을 다룰 때, 원본은 보존하는 게 좋다..! */
     function changeTitle(){
         const arr = [...title]
         arr[0] = "여자 코트 추천" //원본이 아니라 copy본을
@@ -24,6 +66,10 @@ function App() {
         setTitle(arr)*/
         const arr = [...(title.sort())];
         setTitle(arr)
+    }
+
+    const handleModal = ()=>{
+        setModal(!modal)
     }
 
     return (
@@ -40,11 +86,16 @@ function App() {
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{title[2]}</h4>
+                <h4 onClick={handleModal}>{title[2]}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <button onClick={changeTitle}>변경</button>
             <button onClick={sortList}>정렬</button>
+
+            {/*STEP3*/}
+            {
+                modal ? <Modal/> : null
+            }
         </div>
     );
 }
